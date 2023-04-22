e40078633a0a46799b43b70f67d05a23413ae35f
custozza <thomas.duncan@gmx.at>
2023-04-23 00:23:00 +0200

    refactor extract fnEvaluationByThesis.js

---
+++ system/fnEvaluationByThesis.js
@@ -0,0 +1,113 @@
+import {
+	arQuestionsLong,
+	arQuestionsShort,
+	arPersonalPositions,
+	arParties,
+} from './globals.js'
+
+import {
+	fnTransformPositionToButton,
+	fnTransformPositionToIcon,
+	fnTransformPositionToText,
+} from './fnTransform.js';
+
+export function fnEvaluationByThesis() {
+    buildAnswers();
+}
+
+function buildAnswers() {
+
+    for (let i = 0; i < arQuestionsLong.length; i++) {
+        const temp = createQuestionGroupCard(i);
+        $("#resultsByThesis").append(temp);
+        const content = document.createElement('div');
+        content.setAttribute('class', 'collapsible-question');
+        content.innerHTML = createPartyAnswers(i);
+        $("#resultsByThesis").append(content);
+
+    }
+    createCollabsible();
+    createFoldableText();
+}
+
+function createCollabsible() {
+    var coll = document.getElementsByClassName("question-group-header");
+    for (let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var target = this.nextElementSibling;
+            const isOpen = $(target).hasClass('section-expanded');
+            $('.collapsible-question').removeClass('section-expanded');
+            setTimeout(() => {
+                $('#resultsByThesis').css('maxHeight', "100%");
+                console.log('sometime latter');
+            }, 0);
+            if (isOpen) {
+                return
+            }
+            $(target).toggleClass('section-expanded');
+        });
+    }
+}
+function createFoldableText() {
+    $('.opinion').click((e) => {
+        console.log(e.target);
+        $(e.target).toggleClass('clamped');
+    });
+}
+
+function createQuestionGroupCard(i) {
+
+    var positionButton = fnTransformPositionToButton(arPersonalPositions[i]);
+    var positionIcon = fnTransformPositionToIcon(arPersonalPositions[i]);
+    var positionText = fnTransformPositionToText(arPersonalPositions[i]);
+
+    var button = `<button type='button' id='' class='btn ${positionButton} btn-sm selfPosition${i}' 
+	alt=' ${TEXT_ANSWER_USER} : ${positionText}' title='${TEXT_ANSWER_USER}  : ${positionText}'> ${positionIcon} </button>`;
+
+
+    return `
+	<div id="btn-question-group-${i}" class="question-group-header border rounded mow-row-striped">
+		${button} 
+		<div class="btn-question-title"><h2><strong>${arQuestionsShort[i]}</strong><h2></div>
+	</div>`
+}
+
+function createPartyAnswers(questionId) {
+    var result = '';
+
+    for (let i = 0; i < arParties.length; i++) {
+        let party = arParties[i];
+        let weightedPosition = party.answers[questionId].position;
+        let position = Math.sign(weightedPosition);
+        var positionButton = fnTransformPositionToButton(position);
+        var positionIcon = fnTransformPositionToIcon(position);
+        var positionText = fnTransformPositionToText(position);
+
+        let weight = Math.abs(weightedPosition)
+
+        var button = `<button type='button' id='' class='btn ${positionButton} btn-sm alt='${positionText}' title='${positionText}'> ${positionIcon} </button>`;
+
+        var chevronUp = buildChevron(weight);
+
+        result += `
+		<div class="party-answer"> 
+			<div class="party-answer-group ">
+				${button} 
+				${chevronUp}
+			</div>
+			<span class="opinion clamped"><strong>${party.partyShort}</strong>: ${party.answers[questionId].opinions}</span>
+		 </div>`
+    }
+    return `<div class="parties-group questionGroup collapsible-container">${result}</div>`
+}
+
+function buildChevron(weight) {
+    var chevron = `<div class="triangle-container">`
+    chevron += weight > 0 ? `<span class="triangle" style="margin-top: 0;">&#8963;</span>` : "";
+    chevron += weight > 1 ? `<span class="triangle" sylte="margin-top: -39px; margin-bottom: -17px;">&#8963;</span>` : "";
+    chevron += weight > 2 ? `<span class="triangle">&#8963;</span>` : "";
+    chevron += `</div>`;
+    return chevron;
+}
+
