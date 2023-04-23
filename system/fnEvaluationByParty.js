784e1202e2bb87b96480f111452455ba6403f210
custozza <thomas.duncan@gmx.at>
2023-04-23 22:10:59 +0200

    add new chevrons

--- system/fnEvaluationByParty.js
+++ system/fnEvaluationByParty.js
@@ -1,8 +1,5 @@
 import { fnTransformPositionToButton, fnTransformPositionToIcon, fnTransformPositionToText } from "./fnTransform.js";
 import { DEBUGGING, arParties, arPersonalPositions, arQuestionsShort, arSortParties, questionWeight} from "./globals.js";
-import { buildChevron } from "./util.js";
-
-
 
 export function fnEvaluationByParty() {
 
@@ -131,5 +128,15 @@ function partyPositionElementsAsText(weightedPosition) {
     var chevronUp = buildChevron(weight);
 
     return [button, chevronUp];
+}
+
+function buildChevron(weight) {
+    switch(weight) {
+        case 3: return `<button class="triangle-container btn-primary btn-sm btn" style="transform: rotate(270deg)"> ⟫ </button>`;
+        case 2:return `<button class="triangle-container btn-info btn-sm btn" style="transform: rotate(270deg)"> ⟩ </button>`;
+        case 1: return `<button class="triangle-container btn-secondary btn-sm btn" style="transform: rotate(270deg)"> | </button>`;
+        case 0: return `<button class="triangle-container btn-warning btn-sm btn" style=""> ∅ </button>`;
+        default: throw Error('unknown weight');
+    };
 }
 
