9c38310463c1f881f6cdccfebeb7d3df275e5043
custozza <thomas.duncan@gmx.at>
2023-04-23 23:05:23 +0200

    adjust icons in standpunkte

--- system/util.js
+++ system/util.js
@@ -1,10 +1,11 @@
 export function buildChevron(weight) {
-    var chevron = `<div class="triangle-container">`
-    chevron += weight > 0 ? `<span class="triangle" style="margin-top: 0;">&#8963;</span>` : "";
-    chevron += weight > 1 ? `<span class="triangle" sylte="margin-top: -39px; margin-bottom: -17px;">&#8963;</span>` : "";
-    chevron += weight > 2 ? `<span class="triangle">&#8963;</span>` : "";
-    chevron += `</div>`;
-    return chevron;
+    switch(weight) {
+        case 3: return `<button class="triangle-container btn-primary btn-sm btn" style="transform: rotate(270deg)"> ⟫ </button>`;
+        case 2:return `<button class="triangle-container btn-info btn-sm btn" style="transform: rotate(270deg)"> ⟩ </button>`;
+        case 1: return `<button class="triangle-container btn-secondary btn-sm btn" style="transform: rotate(270deg)"> | </button>`;
+        case 0: return `<button class="triangle-container btn-warning btn-sm btn" style=""> ∅ </button>`;
+        default: throw Error('unknown weight');
+    };
 }
 
 export function createFoldableText(querySelector) {
