6f3bc1b8d4cf461bc592b788954cb4833c24847f
LolaRuns <lolaruns@gmx.at>
2023-04-24 20:33:27 +0200

    send votes, weights and store them to db

--- extras/statistics/vote_db.php
+++ extras/statistics/vote_db.php
@@ -8,6 +8,9 @@
 	include "db_password.php";
 
 
+echo "<br>Mat-o-Wahl: Read password from file ".$password;
+
+
 // Establish Connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -18,41 +21,52 @@
 
 // DE: IP-Adresse des Besuchers
 // EN: Ip address of visitor
-	if (getenv("HTTP_X_FORWARDED_FOR")) { 
+	if (getenv("HTTP_X_FORWARDED_FOR")) {
 		$ip=getenv("HTTP_X_FORWARDED_FOR"); }
 	else {
 		$ip=getenv("REMOTE_ADDR"); }
 
 	$ip = explode (',', $ip);
 	$ip = $ip[0];
-	
+
 // DE: Wenn keine IP gespeichert werden soll, einfach die folgende Zeile auskommentieren.
-// Hierbei wird das soeben erhaltene Ergebnis von "$ip" mit einem "Null-Wert" überschrieben 
+// Hierbei wird das soeben erhaltene Ergebnis von "$ip" mit einem "Null-Wert" überschrieben
 // aber das Format für die simple Auswertung (results.html) bleibt erhalten.
 // -> EMPFOHLENE EINSTELLUNG (gemäß DSGVO), da politische Meinungen abgefragt werden.
 //
 // EN: If you do not wish to save any IP addresses (privacy) please uncomment the following line.
 // It replaces the result of "$ip" with zeros but keeps the right format.
-// -> SUGGESTED SETTING (according to EDPR) because you're asking for political views 
-	$ip="0.0.0.0";	
-	
+// -> SUGGESTED SETTING (according to EDPR) because you're asking for political views
+	$ip="0.0.0.0";
 
-// $timestamp = time(); // Unix-Zeitstempel 
-// $timestamp = date("Y-m-d H:i:s"); ausführliches Datumsformat -> nicht empfohlen, da rückverfolgbar über ACCESS.LOG 
+
+// $timestamp = time(); // Unix-Zeitstempel
+// $timestamp = date("Y-m-d H:i:s"); ausführliches Datumsformat -> nicht empfohlen, da rückverfolgbar über ACCESS.LOG
 	$timestamp = date("Y-m-d");
 
-// Sanitize User Input
-	$mowPersonalValues = mysqli_real_escape_string($conn, $_GET["mowpersonal"]);
-	$mowPartiesValues = mysqli_real_escape_string($conn, $_GET["mowparties"]);
+	$votes = implode(',', $_GET["votes"]);
+	$weights = implode(',', $_GET["weights"]);
 
-// Prepare and execute SQL Statement 
-	$sql = "INSERT INTO `$tablename` (ip, timestamp, personal, parties) VALUES ('$ip', '$timestamp', '$mowPersonalValues', '$mowPartiesValues')";
+echo "<br>Mat-o-Wahl: imploded votes".$votes;
+echo "<br>Mat-o-Wahl: imploded weights".$weights;
+
+// Sanitize User Input
+//	$votesSanitized = mysqli_real_escape_string($conn, $_GET["votes"]);
+//	$weightsSanitized = mysqli_real_escape_string($conn, $_GET["weights"]);
+
+//echo "<br>Mat-o-Wahl: sanitized votes".$votesSanitized;
+//echo "<br>Mat-o-Wahl: sanitized weights".$weightsSanitized;
+
+// Prepare and execute SQL Statement
+	$sql = "INSERT INTO `$tablename` (ip, timestamp, personal, parties) VALUES ('$ip', '$timestamp', '$votes', '$weights')";
+
+echo "<br>Mat-o-Wahl: created insert ".$sql;
 
 // Send data
 	if ($conn->query($sql) === TRUE) {
-	  echo "Mat-o-Wahl: New record created successfully into table ".$tablename;
+	  echo "<br>Mat-o-Wahl: New record created successfully into table ".$tablename;
 	} else {
-	  echo "Mat-o-Wahl: Error: <strong>" . $sql . " </strong> <br /> Error-Code:" . $conn->error;
+	  echo "<br>Mat-o-Wahl: Error: <strong>" . $sql . " </strong> <br /> Error-Code:" . $conn->error;
 	}
 
 // Close connection
